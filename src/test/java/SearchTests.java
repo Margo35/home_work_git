217ccb8e0eb34650d4b9a21fa707544dee73a1d1
Puldas Margo <puldas_margarita@mail.ru>
2024-09-04 21:35:00 +0600

    hm 1 - done

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        sleep(50000);
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
+}
